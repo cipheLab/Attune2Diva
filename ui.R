53e8003737870b50fd51291ed6e2b047a966f798
cipheLab <37363841+cipheLab@users.noreply.github.com>
2025-06-16 08:24:52 +0200

    Update ui.R

--- ui.R
+++ ui.R
@@ -2,26 +2,30 @@ library(shiny)
 library(shinydashboard)
 library(shinyjs)
 library(shinybusy)
-ui <- dashboardPage(skin = "black",
+library(flowCore)
+
+ui <- dashboardPage(
+  skin = "black",
   dashboardHeader(title = "FCS Converter"),
   dashboardSidebar(disable = TRUE),
   dashboardBody(
     add_busy_spinner(spin = "fading-circle"),
-    includeCSS("www/styles.css"),  # Include the CSS file
+    includeCSS("www/styles.css"),
     useShinyjs(),
+    
     fluidRow(
       column(12,
-               box(
-                 id = "info_box",
-                 title = "About this tool",
-                 status = "primary",
-                 solidHeader = TRUE,
-                 collapsible = TRUE,
-                 collapsed = T,
-                 "This tool is designed to convert FCS files from the ThermoFisher Attune NxT format to a format compatible with BD FACSDiva software.
-                  Users can upload files from their local machine. Once uploaded, files are processed and converted, and the user
-                  is then able to download the converted files. A log of actions taken during a session is also available for users to view."
-               ),
+             box(
+               id = "info_box",
+               title = "About this tool",
+               status = "primary",
+               solidHeader = TRUE,
+               collapsible = TRUE,
+               collapsed = TRUE,
+               "This tool is designed to convert FCS files from the ThermoFisher Attune NxT format to a format compatible with BD FACSDiva software.
+                Users can upload files from their local machine. Once uploaded, files are processed and converted, and the user
+                is then able to download the converted files. A log of actions taken during a session is also available for users to view."
+             ),
              box( 
                id = "log_box",
                title = "Log Messages",
@@ -29,9 +33,11 @@ ui <- dashboardPage(skin = "black",
                solidHeader = TRUE,
                collapsible = TRUE,
                collapsed = TRUE,
-               verbatimTextOutput("log") 
+               verbatimTextOutput("log")
              )
-      )),
+      )
+    ),
+    
     fluidRow(
       column(
         width = 12,
@@ -44,15 +50,14 @@ ui <- dashboardPage(skin = "black",
         )
       )
     ),
+    
     fluidRow(
       hidden(
         div(
           id = "download_box",
           box(
             title = "Download FCS Files",
-            actionButton("download_button", "Download Converted Files", class = "shine"),
-            downloadButton("download", label = "", class = "invisible"),
-            
+            downloadButton("download", "Download Converted Files", class = "shine")
           )
         )
       )
